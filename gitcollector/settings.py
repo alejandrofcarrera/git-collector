0348863ca3a69418dc3c8fdba61eda637bc918b3
Alejandro Fdez Carrera <alej4fc@gmail.com>
2016-03-16 12:30:33 +0100

    add AMQP environment

--- gitcollector/settings.py
+++ gitcollector/settings.py
@@ -38,6 +38,7 @@ GC_VERSION = "1.0.0"
 GC_LONGNAME = "Git Collector"
 GC_DELAY = int(os.environ.get("GC_DELAY", 60 * 60 * 3))
 GC_IP = os.environ.get("GC_LISTEN_IP", "0.0.0.0")
+GC_HOST = os.environ.get("GC_HOST", "127.0.0.1")
 GC_PORT = int(os.environ.get("GC_LISTEN_PORT", 5000))
 
 # Collector Temporal Configuration
@@ -55,6 +56,12 @@ GC_DB_BRANCHES = int(os.environ.get("GC_BRANCH", 2))
 GC_DB_COMMITS = int(os.environ.get("GC_COMMIT", 3))
 GC_DB_BRANCH_COMMIT = int(os.environ.get("GC_BRANCH_COMMIT", 4))
 GC_DB_COMMITTER_COMMIT = int(os.environ.get("GC_COMMITTER_COMMIT", 5))
+
+# Collector - AMQP
+GC_AMQP_BROKER_HOST = os.environ.get("GC_MQ_IP", "127.0.0.1")
+GC_AMQP_BROKER_PORT = int(os.environ.get("GC_MQ_PORT", 5672))
+GC_AMQP_VIRTUAL_HOST = os.environ.get("GC_MQ_VHOST", "/")
+GC_AMQP_EXCNAME = os.environ.get("GC_MQ_EXCNAME", "gitcollector")
 
 # Collector Password Auto-generated
 GC_USE_PASSWORD = True
